79dd8974ac5fb0fa1723594d711db6c357751333
Ad1L2005 <adil.nurgozha@gmail.com>
2025-04-07 17:34:32 +0500

    dobavil paru metodov

--- app/src/main/java/com/example/labsegiz/MyService.java
+++ app/src/main/java/com/example/labsegiz/MyService.java
@@ -1,10 +1,36 @@
 package com.example.labsegiz;
 
 import android.app.NotificationManager;
+import android.app.Service;
+import android.content.Intent;
 import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
 
-public class MyService {
+import androidx.annotation.Nullable;
+
+public class MyService extends Service {
     private MediaPlayer soundPlayer;
     private static final String CHANNEL_ID = "foreground_service_channel";
     private NotificationManager notificationManager;
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (soundPlayer != null) {
+            soundPlayer.stop();
+            soundPlayer.release();
+            soundPlayer = null;
+        }
+        Toast.makeText(this, "Сервис остановлен", Toast.LENGTH_SHORT).show();
+        Log.i("MyService", "Foreground service stopped...");
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
 }
--- app/src/main/java/com/example/labsegiz/RandomCharacterService.java
+++ app/src/main/java/com/example/labsegiz/RandomCharacterService.java
@@ -1,7 +1,37 @@
 package com.example.labsegiz;
 
-public class RandomCharacterService {
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+public class RandomCharacterService extends Service {
     private boolean isRandomGeneratorOn;
     private final String TAG = "RandomCharacterService";
     private final char[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+
+    public static final String ACTION_TAG = "my.custom.action.tag.lab6";
+
+    private void stopRandomGenerator() {
+        isRandomGeneratorOn = false;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        stopRandomGenerator();
+        Toast.makeText(getApplicationContext(), "Service Stopped", Toast.LENGTH_SHORT).show();
+        Log.i(TAG, "Service destroyed...");
+    }
+
+
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
 }
