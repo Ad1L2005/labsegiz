4bbcaa0c06fb8fbce0c20d95d73769f27bf8d929
Ad1L2005 <adil.nurgozha@gmail.com>
2025-04-07 17:47:27 +0500

    dobavil metody dlia main

--- app/src/main/java/com/example/labsegiz/MainActivity.java
+++ app/src/main/java/com/example/labsegiz/MainActivity.java
@@ -1,24 +1,48 @@
 package com.example.labsegiz;
-
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText randomCharacterEditText;
+    private BroadcastReceiver broadcastReceiver;
+    private Intent serviceIntent;
+    public static final String ACTION_TAG = "my.custom.action.tag.lab6";
+
+    public void onClickMusic(View view) {
+        Intent musicServiceIntent = new Intent(MainActivity.this, MyService.class); // Intent для музыкального сервиса
+        ContextCompat.startForegroundService(MainActivity.this, musicServiceIntent); // Запуск foreground сервиса
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        }); // kdjcbskjcbsdk
+    protected void onStart() {
+        super.onStart();
+        IntentFilter intentFilter = new IntentFilter(ACTION_TAG);
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.S) {
+            intentFilter.setPriority(0);
+        }
+        registerReceiver(broadcastReceiver, intentFilter, Context.RECEIVER_EXPORTED);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(broadcastReceiver);
+    }
+
+    class MyBroadcastReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            char data = intent.getCharExtra("randomCharacter", '?');
+            randomCharacterEditText.setText(String.valueOf(data));
+        }
     }
 }
